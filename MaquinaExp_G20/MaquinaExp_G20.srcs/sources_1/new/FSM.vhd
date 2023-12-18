8fec5a7a4e0f86feaf439410b7bdfc976d02266a
DiegoRPeinado <diego.rpeinado@alumnos.upm.es>
2023-12-18 22:52:49 +0100

    añadido fsm

--- MaquinaExp_G20/MaquinaExp_G20.srcs/sources_1/new/FSM.vhd
+++ MaquinaExp_G20/MaquinaExp_G20.srcs/sources_1/new/FSM.vhd
@@ -44,7 +44,62 @@ end FSM;
 
 architecture Behavioral of FSM is
 
+type STATES is (S0,S1,S2,S3);
+signal CURRENT_STATE: STATES := S0;
+signal NEXT_STATE: STATES;
+
 begin
 
+    state_register: process(RESET, CLK)
+    begin
+        if RESET = '0' then
+            CURRENT_STATE <= S0;
+        elsif rising_edge(CLK) then
+            CURRENT_STATE <= NEXT_STATE;
+        end if;
+    end process;
+    
+    nextstate: process(PAGAR, PAGO_OK, ERROR_COUNTER, CURRENT_STATE)
+    begin
+        NEXT_STATE <= CURRENT_STATE;
+        case CURRENT_STATE is 
+            when S0 =>
+                if PAGAR = '1' then
+                    NEXT_STATE <= S1;
+                end if;                
+            when S1 =>
+                if PAGO_OK = '1' then
+                    NEXT_STATE <= S2;
+                elsif ERROR_COUNTER = '1' then
+                    NEXT_STATE <= S3;
+                end if;                
+            when S2 =>
+                if PAGAR = '0' then
+                    NEXT_STATE <= S0;
+                end if;           
+            when S3 =>
+                if RESET = '1' then
+                    NEXT_STATE <= S0;
+                end if;            
+        end case;
+    end process;
+    
+    output_control: process(CURRENT_STATE)
+    begin
+        case CURRENT_STATE is 
+            when S0 =>
+                ERROR <= '0';
+                REFRESCO_OUT <= '0';
+            when S1 =>
+                ERROR <= '0';
+                REFRESCO_OUT <= '0';
+            when S2 =>
+                ERROR <= '0';
+                REFRESCO_OUT <= '1';
+            when S3 =>
+                ERROR <= '1';
+                REFRESCO_OUT <= '0';
+        end case;
+    end process;
 
 end Behavioral;
