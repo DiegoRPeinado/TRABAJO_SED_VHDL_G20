932cb480f0b74915bf268bc137d31e9e1e4ba5d4
monicasacristans <monica.sacristans@alumnos.upm.es>
2023-12-19 16:15:50 +0100

    modificacion comentarios

--- MaquinaExp_G20/MaquinaExp_G20.srcs/sources_1/new/COUNTER.vhd
+++ MaquinaExp_G20/MaquinaExp_G20.srcs/sources_1/new/COUNTER.vhd
@@ -8,7 +8,7 @@ entity COUNTER is
   CLK: in std_logic;
   CE: in std_logic;
   RESET: in std_logic;
-  MONEDAS: in std_logic_vector(4 downto 0);
+  MONEDAS: in std_logic_vector(3 downto 0);
   --TIPO_REFRESCO: in std_logic;
   ERROR: out std_logic;
   PAGO_OK: out std_logic
